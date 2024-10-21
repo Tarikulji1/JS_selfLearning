a84aebb180c3cc2ff9fba8f8fbb123b375315957
Tarikul Islam <99996136+Tarikulji1@users.noreply.github.com>
2024-10-21 09:24:35 +0000

    Ultimate Color Generator Websites Create start

---
+++ 07_projects/UnlimitedColors/index.js
@@ -0,0 +1,11 @@
+// generator a random color
+
+const randomColor = function(){
+    const hex = "0123456789ABCDEF"
+    let color = '#'
+    for(let i = 0; i < 6 ; i++){
+
+    }
+}
+
+parseInt(Math.random() * 16)
