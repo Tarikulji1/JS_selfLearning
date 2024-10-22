558258cdf1f9053e6006cd96c87c0eb50803c526
Tarikul Islam <99996136+Tarikulji1@users.noreply.github.com>
2024-10-22 01:32:46 +0000

    promise 5 and fetch() api

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -52,3 +52,47 @@ promisesFour.then((user) => {
     console.log(error);
 }).finally(() => console.log("The promise is resolved or rejected") )
 
+
+const promisesFive = new Promise(function(resolve, reject){
+    setTimeout(function (){
+        let error = true
+        if(!error){
+            resolve({username: "javascript", password: "456"})
+        } else {
+            reject('ERROR: JS user went wrong')
+        }
+    }, 1000)
+});
+
+async function consumePromiseFive(){
+    try {
+        const response = await promisesFive
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+};
+consumePromiseFive()
+
+/*
+async function getAllUsers(){
+   try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const data = await response.json()
+        console.log(data);
+   } catch (error) {
+        console.log('E: ', error);
+   }   
+}
+
+getAllUsers()
+*/
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => console.log(error))
